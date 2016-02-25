401e624b759b962bb0374c54e507dbcfaa29a11e
enochlj <xkingh929x@qq.com>
2016-02-25 11:19:14 +0800

    UPDATE

---
+++ src/cn/hlj/shiro/test/Test02.java
@@ -0,0 +1,16 @@
+package cn.hlj.shiro.test;
+
+import org.junit.Test;
+
+public class Test02 {
+
+	@Test
+	public void testA() {
+
+		System.out.println("HELLO testA");
+		
+		System.out.println("HELLO testA - 11:18 FOR PUSH");
+		
+	}
+
+}
