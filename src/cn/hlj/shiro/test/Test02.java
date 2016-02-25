579fc7ee452c6eaede94bef4a14ec8ed5d8f369b
enochlj <xkingh929x@qq.com>
2016-02-25 14:09:48 +0800

    UPDATE

--- src/cn/hlj/shiro/test/Test02.java
+++ src/cn/hlj/shiro/test/Test02.java
@@ -8,9 +8,16 @@ public class Test02 {
 	public void testA() {
 
 		System.out.println("HELLO testA");
-		
+
 		System.out.println("HELLO testA - 11:18 FOR PUSH");
-		
+
+	}
+
+	@Test
+	public void testB() {
+		System.out.println("HELLO testB");
+
+		System.out.println("HELLO testB - 13:53 FOR PUSH");
 	}
 
 }
